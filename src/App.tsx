3b8f8402de614e029ad0d771c23b4d81b3aae33c
Marcos Guibso <mgss.lic17@uea.edu.br>
2022-06-25 17:37:22 -0300

    feat: Adicionando compnente video e vesão mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
-import { gql, useQuery } from "@apollo/client"
+import { ApolloProvider, gql, useQuery } from "@apollo/client"
 import { useEffect } from "react"
+import { BrowserRouter } from "react-router-dom"
 import { Header } from "./components/Header"
 import { Lesson } from "./components/Lesson"
 import { Sidebar } from "./components/Sidebar"
 import { Video } from "./components/Video"
 import { client } from "./lib/apollo"
 import { Event } from "./pages/Event"
+import { Router } from "./Router"
 
 
 function App() {
   
-  
-
   return (
-    <div>
-      <Event />
-    </div>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>      
+    </ApolloProvider>
     
   )
 }
--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,49 +1,53 @@
-import { CheckCircle, Lock } from 'phosphor-react'
-import { isPast, format } from 'date-fns'
-import ptBR from 'date-fns/locale/pt-BR'
+import { CheckCircle, Lock } from 'phosphor-react';
+import { isPast, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import { Link } from 'react-router-dom';
 
 interface LessonProps {
-    title: string;
-    slug: string;
-    availableAt: Date;
-    type: 'live' | 'class';
+  title: string;
+  slug: string;
+  availableAt: Date;
+  type: 'live' | 'class';
 }
 
 export function Lesson(props: LessonProps) {
-    const isLessonAvailable = isPast(props.availableAt);
-    const availabledDateFormated = format(props.availableAt, "EEEE' • 'd' de ' MMMM' • 'k'h'mm ", {
-        locale: ptBR
-    })
+  const isLessonAvailable = isPast(props.availableAt);
 
-    return (
-        <a href="#">
-            <span className="text-gray-300">
-                {availabledDateFormated}
+  const availableDateFormatted = format(props.availableAt, "EEEE' • 'd' de 'MMMM' • 'k'h'mm", {
+    locale: pt, 
+  });
+
+  const availableDateFormattedWithCapitalized = availableDateFormatted.charAt(0).toUpperCase() + availableDateFormatted.slice(1);
+
+  return (
+    <Link to={`/event/lesson/${props.slug}`} className="group">
+      <span className="text-gray-300">
+        {availableDateFormattedWithCapitalized}
+      </span>
+
+      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
+        <header className="flex items-center justify-between">
+          {isLessonAvailable ? (
+            <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+              <CheckCircle size={20} />
+              Conteúdo liberado
             </span>
+          ) : (
+            <span className="text-sm text-orange-500 font-medium flex items-center gap-2">
+              <Lock size={20} />
+              Em breve
+            </span>
+          )}
+          
+          <span className="text-xs rounded py-[0.125rem] px-2 text-white border border-green-300 font-bold">
+            {props.type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
+          </span>
+        </header>
 
-            <div className="rounded border border-gray-500 p-4 mt-2">
-                <header className="flex items-center justify-between">
-                    {isLessonAvailable ? (
-                        <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
-                            <CheckCircle size={20} />
-                            Conteúdo liberado
-                        </span>
-                    ) : (
-                        <span className="text-sm text-orange-500 font-medium flex items-center gap-2">
-                            <Lock size={20} />
-                            Em breve
-                        </span>
-                    )}
-
-                    <span className="text-xs rounded py-[0.125rem] px-2 text-white border border-green-300">
-                        {props.type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
-                    </span>
-                </header>
-
-                <strong className="text-gray-200 mt-5 block">
-                    {props.title}
-                </strong>
-            </div>
-        </a>
-    )
+        <strong className="text-gray-200 mt-5 block">
+          {props.title}
+        </strong>
+      </div>
+    </Link>
+  )
 }
